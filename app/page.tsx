54eb6c4515aeae6c1100d082ba0e1f4ec9bc69e5
lilyrren <lilyyren@gmail.com>
2024-11-28 13:05:20 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+// where you want users to do something 
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-emerald-500 text-white">
+        Click Me 
+      </Button>
+    </div>
+  );
+}
